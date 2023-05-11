cd3b147940ecdac26825b9443be1c23da57f39fa
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-05-11 21:33:49 +0530

    ADD:sortout some bug

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -107,7 +107,7 @@ const RegisterModal = () => {
                 "
       >
         <div className="justify-center flex flex-row items-center gap-2">
-          <div>Already have account ?</div>
+          <div>Already have an account ?</div>
           <div
             onClick={toggle}
             className="
